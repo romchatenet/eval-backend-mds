f0d4d86698375dd309a695acb1bfc704860431ed
romchatenet <62374971+romchatenet@users.noreply.github.com>
2020-06-26 11:13:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,23 @@ index.php
 
 <form action="action.php" method="post">
 *on indique que ce fichier est un "form" qui enverra les informations récupérées sur le serveur*
- <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre nom : <input type="text" name="rcnom" /></p>
 
- <p>Votre ville : <input type="text" name="ville" /></p>
+ <p>Votre ville : <input type="text" name="rcville" /></p>
 
- <p>adresse mail : <input type="text" name="mail" /></p>
+ <p>adresse mail : <input type="text" name="rcmail" /></p>
 
- <p>Votre numéro de téléphone : <input type="text" name="tel" /></p>
-
+ <p>Votre numéro de téléphone : <input type="text" name="rctel" /></p>
+	<select name="rcproj" >
 *plus haut chaque ligne collecte une information qu'elle classe par la suite en lui donnant un nom*
- <p><input type="submit" value="OK"></p>
+
+<label for="rcproj">L'objectif de votre projet</label>
+<select name="rcproj" >
+  <option value="Wordpress">Wordpress</option>
+  <option value="Vue.js">Vue.js</option>
+</select><br>
+*les 5 lignes au dessus désignent une séction avec box à cocher. On choisi entre les 2 options et on coche celle souhaitée. Par la suite le choix est sauvegardé selon l'opion choisie. 
+ <p><input type="submit" name="rcproj" value="OK"></p>
 *ici la commmande d'envoi des informations sur le serveur*
 </form>
 *fermeture de la balise "form" qui signale la fin du formulaire
